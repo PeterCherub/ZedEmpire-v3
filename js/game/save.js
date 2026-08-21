04c3c95ffd19c93ab2cfa5eca8bc43ddf1c509db
PeterRoc <129948887+PeterCherub@users.noreply.github.com>
2026-08-21 04:36:49 +0200

    Delete js directory

--- js/game/save.js
+++
@@ -1,5 +0,0 @@
-const KEY="zedempire-save-v2";
-export const saveExists=()=>!!localStorage.getItem(KEY);
-export const writeSave=(data)=>localStorage.setItem(KEY,JSON.stringify(data));
-export const readSave=()=>{try{return JSON.parse(localStorage.getItem(KEY));}catch{return null;}};
-export const deleteSave=()=>localStorage.removeItem(KEY);
